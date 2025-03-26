e52c03d29cb1534cbb207bfe7748176d1d378626
teishahbc <121613187+teishahbc@users.noreply.github.com>
2025-03-26 16:29:49 +0800

    Update vuepress.config.ts

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -44,7 +44,7 @@ export default defineUserConfig({
       downProxy: cloudflarePagesDownProxy(),
     },
     {
-      mountPath: "/Mihomo-Party",
+      mountPath: "/mihomo-party",
       analysis: githubReleasesFilesAnalysis({ user: "pompurin404", repository: "mihomo-party" }),
       // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
       // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
@@ -52,11 +52,11 @@ export default defineUserConfig({
       downProxy: cloudflarePagesDownProxy(),
     },
         {
-      mountPath: "/Mihomo-Party/latest",
-      analysis: githubReleasesFilesAnalysis({ user: "pompurin404", repository: "mihomo-party" }),
-      // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
-      // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
-      // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
+      mountPath: "/Latest",
+      analysis: fileUrlTreeAnalysis({
+        "/V2rayN": "https://github.com/2dust/v2rayN/releases/latest",
+        "/mihomo-party": "https://github.com/mihomo-party-org/mihomo-party/releases/latest"
+      }),
       downProxy: cloudflarePagesDownProxy(),
     },
   ])
